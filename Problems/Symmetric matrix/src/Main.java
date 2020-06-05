75c036557c5628e07a75fb2659c373cc4a38e3d0
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-06-05 11:18:22 -0500

    Add files via upload

---
+++ Problems/Symmetric matrix/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+
+        boolean res = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    res = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(res ? "YES":"NO");
+    }
+}
