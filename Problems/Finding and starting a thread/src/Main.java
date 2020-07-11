dfc318de8f234aed62121bc08fc4a0c2a6d4c2e3
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-07-11 11:22:46 -0500

    Add files via upload

--- Problems/Finding and starting a thread/src/Main.java
+++ Problems/Finding and starting a thread/src/Main.java
@@ -1,5 +1,17 @@
 class ThreadProcessor {
     public static void findAndStartThread(Thread... threads) throws InterruptedException {
         // implement this method
+        int i;
+
+        for (i = 0; i < threads.length; i++) {
+            if (threads[i].getState().equals(Thread.State.NEW)) {
+                threads[i].start();
+                threads[i].join();
+                break;
+
+            }
+        }
+
+
     }
 }
