fa846768af0ce267bb7a08dc70a203de49d5fa29
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-07-15 17:56:22 -0500

    Add files via upload

---
+++ Problems/Companies/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        List list = new ArrayList<>();
+        while (scanner.hasNext()) {
+            list.add(scanner.next());
+        }
+        System.out.println(list);
+
+
+    }
+}
