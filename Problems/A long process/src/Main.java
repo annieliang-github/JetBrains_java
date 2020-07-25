fd699084ef2aa24ba73736f93e0f304b674b2b2d
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-07-25 17:51:13 -0500

    Add files via upload

--- Problems/A long process/src/Main.java
+++ Problems/A long process/src/Main.java
@@ -11,7 +11,28 @@ public class Main {
         message = scanner.nextLine();
         errorCode = Integer.parseInt(scanner.nextLine());
 
-        startLongProcess(/* pass your callback here */);
+        Callback callback = new Callback() {
+            @Override
+            public void onStarted() {
+                System.out.println("The process started");
+            }
+
+            @Override
+            public void onStopped(String cause) {
+                System.out.println(cause);
+            }
+
+            @Override
+            public void onFinished(int code) {
+                if (code == 0) {
+                    System.out.println("The process successfully finished");
+                } else {
+                    System.out.println("The process is finished with error: " + code);
+                }
+            }
+        };
+
+        startLongProcess(callback);
 
     }
 
